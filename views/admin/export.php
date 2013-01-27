6bab4fb3c0abd4ed27f5b4cfadae9b2ce17df6bc
Zac Vineyard <zacvineyard@gmail.com>
2013-01-27 11:09:49 -0700

    Updated wrapper classes for 2.2.

--- views/admin/export.php
+++ views/admin/export.php
@@ -3,9 +3,14 @@
 </section>
 
 <section class="item">
-	<?php echo form_open(); ?>
-	<p><?php echo lang('ie:choose_site'); ?></p>
-	<?php //echo form_dropdown('site', $sites, $this->input->post('site')); ?>
-	<?php $this->load->view('admin/partials/buttons', array('buttons' => array('export'))); ?>
-	<?php echo form_close();?>
+
+	<section class="content">
+
+		<?php echo form_open(); ?>
+		<p><?php echo lang('ie:choose_site'); ?></p>
+		<?php $this->load->view('admin/partials/buttons', array('buttons' => array('export'))); ?>
+		<?php echo form_close();?>
+
+	</section>
+
 </section>
